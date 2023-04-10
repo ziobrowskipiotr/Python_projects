1dd306f816a2889946942e7272bf5c6b24ce9ddd
Piotr <piotrziobrowski03@gmail.com>
2023-04-10 04:01:49 +0200

    test

--- Zadanie1.py
+++
@@ -1,8 +0,0 @@
-for i in range(500, 3001):
-    if i % 7 == 0 and i % 5 != 0:
-        print(str(i))
-
-result = "".join(str(i) for i in range(500, 3001) if i % 7 == 0 and i % 5 != 0)
-print(result)
-print(result.count(str(21)))
-result.replace('21', 'XX')
